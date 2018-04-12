ffb59a7466d746b6c67c8fbffa33e654c972446b
Margus Pala <margus.pala@gmail.com>
2018-04-12 15:32:35 +0300

    Improved stability

--- SmartAccountsClass.php
+++ SmartAccountsClass.php
@@ -1,68 +1,71 @@
 <?php
 
-include_once( 'SmartAccountsClient.php' );
-include_once( 'SmartAccountsSalesInvoice.php' );
-include_once( 'SmartAccountsApi.php' );
-include_once( 'SmartAccountsPayment.php' );
-include_once( 'SmartAccountsArticle.php' );
-include_once( dirname( __FILE__ ) . '/../woocommerce/woocommerce.php' );
+include_once('SmartAccountsClient.php');
+include_once('SmartAccountsSalesInvoice.php');
+include_once('SmartAccountsApi.php');
+include_once('SmartAccountsPayment.php');
+include_once('SmartAccountsArticle.php');
 
-class SmartAccountsClass {
+class SmartAccountsClass
+{
+    public static function orderStatusProcessing($order_id)
+    {
+        //try catch makes sure your store will operate even if there are errors
+        try {
+            $order = wc_get_order($order_id);
+            if (strlen(get_post_meta($order_id, 'smartaccounts_invoice_id', true)) > 0) {
+                return; //Smartaccounts order is already created
+            }
 
-	public static function orderStatusProcessing( $order_id ) {
+            $saClient       = new SmartAccountsClient($order);
+            $client         = $saClient->getClient();
+            $saSalesInvoice = new SmartAccountsSalesInvoice($order, $client);
 
-		//try catch makes sure your store will operate even if there are errors
-		try {
-			$order = new WC_Order( $order_id );
-			if ( strlen( get_post_meta( $order_id, 'smartaccounts_invoice_id', true ) ) > 0 ) {
-				return; //Smartaccounts order is already created
-			}
+            $invoice   = $saSalesInvoice->saveInvoice();
+            $saPayment = new SmartAccountsPayment($order, $invoice);
+            $saPayment->createPayment();
+            update_post_meta($order_id, 'smartaccounts_invoice_id', $invoice['invoice']['invoiceNumber']);
+        } catch (Exception $exception) {
+            $handle = fopen(dirname(__FILE__) . "/error.log", 'a');
 
-			$saClient       = new SmartAccountsClient( $order );
-			$client         = $saClient->getClient();
-			$saSalesInvoice = new SmartAccountsSalesInvoice( $order, $client );
+            if ($handle) {
+                fwrite($handle, new DateTime());
+                fwrite($handle, "Error reason: " . $exception->getMessage());
+                fwrite($handle, $exception->getTraceAsString());
+                fclose($handle);
+            }
+        }
+    }
 
-			$invoice   = $saSalesInvoice->saveInvoice();
-			$saPayment = new SmartAccountsPayment( $order, $invoice );
-			$saPayment->createPayment();
-			update_post_meta( $order_id, 'smartaccounts_invoice_id', $invoice['invoice']['invoiceNumber'] );
-		} catch ( Exception $exception ) {
-			$handle = fopen( dirname( __FILE__ ) . "/error.log", 'a' );
+    function registerSettings()
+    {
+        register_setting('smartaccounts_options', 'sa_api_pk');
+        register_setting('smartaccounts_options', 'sa_api_sk');
+        register_setting('smartaccounts_options', 'sa_api_payment_account');
+        register_setting('smartaccounts_options', 'sa_api_shipping_code', ['default' => 'shipping']);
+    }
 
-			if ( $handle ) {
-				fwrite( $handle, $exception->getMessage() );
-				fwrite( $handle, $exception->getTraceAsString() );
-				fclose( $handle );
-			}
-		}
-	}
+    function optionsPage()
+    {
+        add_submenu_page('woocommerce', 'SmartAccounts settings', "SmartAccounts", 'manage_woocommerce',
+            'SmartAccounts', 'SmartAccountsClass::options_page_html');
+    }
 
-
-	function registerSettings() {
-		register_setting( 'smartaccounts_options', 'sa_api_pk' );
-		register_setting( 'smartaccounts_options', 'sa_api_sk' );
-		register_setting( 'smartaccounts_options', 'sa_api_payment_account' );
-		register_setting( 'smartaccounts_options', 'sa_api_shipping_code', [ 'default' => 'shipping' ] );
-	}
-
-	function optionsPage() {
-		add_submenu_page( 'woocommerce', 'SmartAccounts settings', "SmartAccounts", 'manage_woocommerce', 'SmartAccounts', 'SmartAccountsClass::options_page_html' );
-	}
-
-	function options_page_html() {
-		if ( ! current_user_can( 'manage_options' ) ) {
-			return;
-		}
-		?>
+    function options_page_html()
+    {
+        if ( ! current_user_can('manage_options')) {
+            return;
+        }
+        ?>
         <div class="wrap">
-            <h1><?= esc_html( get_admin_page_title() ); ?></h1>
+            <h1><?= esc_html(get_admin_page_title()); ?></h1>
             <form action="options.php" method="post">
                 <table class="form-table">
                     <tr valign="top">
                         <th>SmartAccounts public key</th>
                         <td>
                             <input name="sa_api_pk" size="50"
-                                   value="<?php echo esc_attr( get_option( 'sa_api_pk' ) ); ?>"/>
+                                   value="<?php echo esc_attr(get_option('sa_api_pk')); ?>"/>
                         </td>
                     </tr>
 
@@ -70,7 +73,7 @@ class SmartAccountsClass {
                         <th>SmartAccounts private key</th>
                         <td>
                             <input name="sa_api_sk" size="50"
-                                   value="<?php echo esc_attr( get_option( 'sa_api_sk' ) ); ?>"/>
+                                   value="<?php echo esc_attr(get_option('sa_api_sk')); ?>"/>
                         </td>
                     </tr>
 
@@ -78,7 +81,7 @@ class SmartAccountsClass {
                         <th>SmartAccounts payments Bank account name</th>
                         <td>
                             <input name="sa_api_payment_account" size="50"
-                                   value="<?php echo esc_attr( get_option( 'sa_api_payment_account' ) ); ?>"/>
+                                   value="<?php echo esc_attr(get_option('sa_api_payment_account')); ?>"/>
                         </td>
                     </tr>
 
@@ -86,21 +89,19 @@ class SmartAccountsClass {
                         <th>SmartAccounts shipping item name</th>
                         <td>
                             <input name="sa_api_shipping_code" size="50"
-                                   value="<?php echo esc_attr( get_option( 'sa_api_shipping_code' ) ); ?>"/>
+                                   value="<?php echo esc_attr(get_option('sa_api_shipping_code')); ?>"/>
                         </td>
                     </tr>
 
                 </table>
-				<?php
+                <?php
 
-				settings_fields( 'smartaccounts_options' );
-				submit_button( 'Salvesta seaded' );
-				?>
+                settings_fields('smartaccounts_options');
+                submit_button('Salvesta seaded');
+                ?>
             </form>
         </div>
-		<?php
-	}
+        <?php
+    }
 
 }
-
-
--- smartaccounts.php
+++ smartaccounts.php
@@ -3,26 +3,38 @@
  * Plugin Name: SmartAccounts
  * Plugin URI: https://github.com/smartman/woocommerce_smartaccounts
  * Description: This plugin creates sales invoices in the smartaccounts.ee Online Accounting Software after Woocommerce order creation
- * Version: 1.1.1
+ * Version: 1.2
  * Author: Margus Pala
  * Author URI: http://marguspala.com
  * License: GPLv2 or later
  * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
  * Requires at least: 4.8.0
- * Tested up to: 4.9.1
+ * Tested up to: 4.9.5
  */
 
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if ( ! defined('ABSPATH')) {
+    exit;
 } // Exit if accessed directly
 
-require_once( 'SmartAccountsClass.php' );
 
-add_action( 'admin_menu', 'SmartAccountsClass::optionsPage' );
+function smartaccounts_missing_wc_admin_notice()
+{
+    ?>
+    <div class="notice notice-error">
+        <p>SmartAccounts is WooCommerce plugin and requires WooCommerce plugin to be installed</p>
+    </div>
+    <?php
+}
 
-add_action( 'admin_init', 'SmartAccountsClass::registerSettings' );
-add_action( 'woocommerce_order_status_processing', 'SmartAccountsClass::orderStatusProcessing' );
-add_action( 'woocommerce_order_status_completed', 'SmartAccountsClass::orderStatusProcessing' );
+if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+    require_once('SmartAccountsClass.php');
 
-//add_action( 'init', 'SmartAccountsClass::orderStatusProcessing' );
+    add_action('admin_menu', 'SmartAccountsClass::optionsPage');
+
+    add_action('admin_init', 'SmartAccountsClass::registerSettings');
+    add_action('woocommerce_order_status_processing', 'SmartAccountsClass::orderStatusProcessing');
+    add_action('woocommerce_order_status_completed', 'SmartAccountsClass::orderStatusProcessing');
+} else {
+    add_action('admin_notices', 'smartaccounts_missing_wc_admin_notice');
+}
